7ae5c022b3036a463183b0264f33eb23bf858940
Nicolas <nicolas.ale.girotti@gmail.com>
2025-09-07 19:53:47 -0300

    Constructor a Punto

--- src/ar/edu/unq/po2/tp3/Punto.java
+++ src/ar/edu/unq/po2/tp3/Punto.java
@@ -7,6 +7,24 @@ public class Punto {
 	private float y;
 	
 	
+	public Punto(float x, float y) {
+		super(); // ¿Por que es necesario este super?
+		this.setXY(x,y);
+	}
+	
+	public void setXY(float x, float y) {
+		this.setX(x);
+		this.setY(y);
+	}
+	
+	private void setX(float x) {
+		this.x = x;
+	}
+	
+	private void setY(float y) {
+		this.y = y;
+	}
+	
 	
 	
 }
